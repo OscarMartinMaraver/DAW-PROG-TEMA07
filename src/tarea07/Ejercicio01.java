30c5669bc94986fdbdc6fc666bac18840c79b9c7
Antonio <Antonio@PORTATILTTL>
2023-03-05 17:29:39 +0100

    Prueba desde el protatil

---
+++ src/tarea07/Ejercicio01.java
@@ -0,0 +1,52 @@
+package tarea07;
+
+/**
+ * Ejercicio 1. Gestionando mascotas
+ * @author Oscar Martín Maraver
+ */
+public class Ejercicio01 {
+    
+    public static void main(String[] args) {
+
+        //----------------------------------------------
+        //    Declaración de variables y constantes
+        //----------------------------------------------
+        
+        // Constantes
+        
+        final int NUMERO_MASCOTAS = 4;
+        
+        //----------------------------------------------
+        //                Entrada de datos 
+        //----------------------------------------------
+        
+        // No se piden datos al usuario, ya que se usa un número fijo de elementos aleatorios
+        System.out.println("CONJUNTOS DE MASCOTAS");
+        System.out.println("---------------------");
+
+        //----------------------------------------------
+        //                  Procesamiento
+        //----------------------------------------------
+        
+        // Rellenamos los conjuntos con aleatorios hasta que haya NUMERO_MASCOTAS
+        
+        
+        // Cálculo de la Unión de los dos conjuntos 
+        
+        
+        // Cálculo de la Intersección de los conjuntos
+        
+        
+        // Cálculo de la Diferencia de los conjuntos
+        
+        
+        //----------------------------------------------
+        //              Salida de Resultados 
+        //----------------------------------------------
+
+
+
+
+    }
+   
+}
