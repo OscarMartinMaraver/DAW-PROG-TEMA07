c7e51862097e14e988c422251b9ebf9172292457
usuario <usuario@ORDENADOR-OSCAR>
2023-03-06 20:17:30 +0100

    Cambios desde PC Oscar

--- src/tarea07/Ejercicio01.java
+++ src/tarea07/Ejercicio01.java
@@ -45,7 +45,7 @@ public class Ejercicio01 {
             m= Utilidades.mascotaAleatoria();
             n= Utilidades.mascotaAleatoria();
             if (!conjuntoUno.add(m)||!conjuntoDos.add(n)) {
-                System.out.println("La masota ya se encuentra en el conjunto");
+                System.out.println("La mascota ya se encuentra en el conjunto");
             } else {
                 conjuntoUno.add(m);
                 conjuntoDos.add(n);
@@ -55,13 +55,22 @@ public class Ejercicio01 {
 
         // Cálculo de la Unión de los dos conjuntos 
         
+        HashSet<Mascota> union=new HashSet<>();
+        union.addAll(conjuntoUno);
+        union.addAll(conjuntoDos);
         
         // Cálculo de la Intersección de los conjuntos
         
+        HashSet<Mascota> interseccion=new HashSet<>();
+        interseccion.addAll(union);
+        interseccion.retainAll(conjuntoUno);
         
         // Cálculo de la Diferencia de los conjuntos
         
-        
+        HashSet<Mascota> diferencia=new HashSet<>();
+        diferencia.addAll(union);
+        diferencia.removeAll(conjuntoUno);
+
         //----------------------------------------------
         //              Salida de Resultados 
         //----------------------------------------------
@@ -76,10 +85,23 @@ public class Ejercicio01 {
         //O imprimir la lista directamete
         System.out.print(conjuntoUno.toString());
         
+        
         System.out.println();
         System.out.println("Conjunto Mascotas2:");
         
         System.out.print(conjuntoDos.toString());
+
+        System.out.println();
+        System.out.println("Union Mascotas1 y Mascotas2:");
+        System.out.print(union.toString());
+        
+        System.out.println();
+        System.out.println("Intersección Unión y Mascotas1:");
+        System.out.print(interseccion.toString());
+        
+        System.out.println();
+        System.out.println("Diferencia Unión-Mascotas1:");
+        System.out.print(diferencia.toString());
 
     }
    
--- src/tarea07/Ejercicio02.java
+++ src/tarea07/Ejercicio02.java
@@ -1,7 +1,9 @@
 package tarea07;
 
+import java.util.ArrayList;
+
 /** Ejercicio 2. Clasificación de mascotas
- * @author Profesor
+ * @author Oscar Martín Maraver
  */
 public class Ejercicio02 {
 
@@ -26,23 +28,78 @@ public class Ejercicio02 {
 
         // Rellenamos la lista con mascotas aleatorias hasta que haya NUMERO_MASCOTAS
 
+        ArrayList<Mascota> arrayMascota = new ArrayList<>();
 
+        int contadorMascota = 0;
+
+        do {
+            Mascota m;
+            m = Utilidades.mascotaAleatoria();
+            arrayMascota.add(m);
+            contadorMascota++;
+
+        } while (contadorMascota < NUMERO_MASCOTAS);
         
         //----------------------------------------------
         //               Procesamiento
         //----------------------------------------------
         
+        //Clasificación por tipo PERRO
+        ArrayList<Mascota> arrayPerro = new ArrayList<>();
+
+        for (Mascota i : arrayMascota) {
+            if ((i.getTipo()).startsWith("P")) {
+                arrayPerro.add(i);
+            } 
+        }
+
+        //Clasificación por tipo GATO
+        ArrayList<Mascota> arrayGato = new ArrayList<>();
+
+        for (Mascota i : arrayMascota) {
+            if ((i.getTipo()).startsWith("G")) {
+                arrayGato.add(i);
+            } 
+        }
         
+        //Clasificación por sexo MACHO
         
+        ArrayList<Mascota> arrayMacho = new ArrayList<>();
+
+        for (Mascota i : arrayMascota) {
+            if ((i.getSexo()).startsWith("M")) {
+                arrayMacho.add(i);
+            } 
+        }
         
+        //Clasificación por sexo MACHO
+        
+        ArrayList<Mascota> arrayHembra = new ArrayList<>();
+
+        for (Mascota i : arrayMascota) {
+            if ((i.getSexo()).startsWith("H")) {
+                arrayHembra.add(i);
+            } 
+        }
         
         //----------------------------------------------
         //            Salida de resultados
         //----------------------------------------------
 
-
-
-        
+        System.out.println("1. Contenido inicial de la lista mascotas:");
+        System.out.print(arrayMascota.toString());
+        System.out.println();
+        System.out.println("3. Contenido final de la lista de perr@s:");
+        System.out.print(arrayPerro.toString());
+        System.out.println();
+        System.out.println("4. Contenido final de la lista de gat@s:");
+        System.out.print(arrayGato.toString());
+        System.out.println();
+        System.out.println("5. Contenido final de la lista de mascotas machos:");
+        System.out.print(arrayMacho.toString());
+        System.out.println();
+        System.out.println("6. Contenido final de la lista de mascotas hembras:");
+        System.out.print(arrayHembra.toString());
     }
 
 }
