900a7daf4f89759ae6c0c42ed4540af9254a9690
usuario <usuario@ORDENADOR-OSCAR>
2023-03-08 15:45:01 +0100

    Cambios desde PC Oscar

--- src/tarea07/Ejercicio05.java
+++ src/tarea07/Ejercicio05.java
@@ -1,7 +1,11 @@
 package tarea07;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 /** Ejercicio 5. Ordenación de mascotas
- * @author Profesor
+ * @author Oscar Martín Maraver
  */
 public class Ejercicio05 {
 
@@ -14,29 +18,110 @@ public class Ejercicio05 {
         //----------------------------------------------
         //               Entrada de datos 
         //----------------------------------------------
-        
+        System.out.println("ORDENACIÓN DE MASCOTAS");
+        System.out.println("----------------------");
         // No se piden datos al usuario, ya que vamos a introducir unos datos concretos
-        
+        ArrayList <Mascota> mascota = new ArrayList<>();
+       
+        String[] caractHappy={"Pelo corto","Tricolor"};
+        String[] caractNube={"Pelo largo", "Blanco", "25kg"};
+        String[] caractTana={"Pelo largo"};
+        String[] caractKhal = {};
+
+        Mascota m= new Mascota ("Happy End", 14,"Gata", "Hembra", "Europeo", caractHappy);
+        Mascota n= new Mascota ( "Nube", 8, "Perro", "Macho", "Samoyedo", caractNube);
+        Mascota o= new Mascota ("Tana", 7, "Gata", "Hembra", "Azul ruso", caractTana);
+        Mascota p= new Mascota ("Khal", 6, "Perro", "Macho", "Akita Inu", caractKhal);
+        mascota.add(m);
+        mascota.add(n);
+        mascota.add(o);
+        mascota.add(p);
 
         
         // Mostramos el contenido inicial de la lista
-        
+        System.out.println("Contenido inicial de la lista:"+"\n");
+           for(Mascota i: mascota){
+           System.out.printf( "%d. %s\n", mascota.indexOf(i)+1, i.toString());
+        }
+        System.out.println();   
         //----------------------------------------------
         //     Procesamiento + Salida de resultados
         //----------------------------------------------
 
         // Ordenación de la lista por nombre de la mascota (alfabético) y la mostramos por pantalla
-
+        
+        Collections.sort(mascota, new ComparadorMascotasPorNombre());
+        
+        System.out.println("Ordenación de la lista por nombre (alfabético):"+"\n");
+        for(Mascota i: mascota){
+           System.out.printf( "%d. %s\n", mascota.indexOf(i)+1, i.toString());
+        }
+        System.out.println(); 
         
         // Ordenación de la lista por edades y la mostramos por pantalla
 
+        Collections.sort(mascota, new ComparadorMascotasPorEdad());
         
+        System.out.println("Ordenación de la lista por edad:"+"\n");
+        for(Mascota i: mascota){
+           System.out.printf( "%d. %s\n", mascota.indexOf(i)+1, i.toString());
+        }
+        System.out.println();
         
         // Ordenación de la lista por número de características de las mascotas y la mostramos por pantalla
-
-
+        
+        Collections.sort(mascota, new ComparadorMascotasPorNumCaracteristicas());
+        
+        System.out.println("Ordenación de la lista por número de características que describen a la mascota:"+"\n");
+        for(Mascota i: mascota){
+           System.out.printf( "%d. %s\n", mascota.indexOf(i)+1, i.toString());
+        }
+        System.out.println();
     }
 
-            
+   static class ComparadorMascotasPorNombre implements Comparator<Mascota> {
+
+        @Override
+        public int compare(Mascota mascota1, Mascota mascota2) {
+            return mascota1.getNombre().compareTo(mascota2.getNombre());
+        }
+    }
+   
+    static class ComparadorMascotasPorEdad implements Comparator<Mascota> {
+
+        @Override
+        public int compare(Mascota mascota1, Mascota mascota2) {
+            int resultado;
+            if (mascota1.getEdad() < mascota2.getEdad()) {
+                resultado = - 1;
+            } else {
+                if (mascota1.getEdad() > mascota2.getEdad()) {
+                    resultado = 1;
+                } else {
+                    resultado = 0;
+                }
+            }
+            return resultado;
+        }
+    }
+   
+    static class ComparadorMascotasPorNumCaracteristicas implements Comparator<Mascota> {
+
+        @Override
+        public int compare(Mascota mascota1, Mascota mascota2) {
+            int resultado;
+            if (mascota1.getCaracteristicas().size() < mascota2.getCaracteristicas().size()) {
+                resultado = -1;
+            } else {
+                if (mascota1.getCaracteristicas().size() > mascota2.getCaracteristicas().size()) {
+                    resultado = 1;
+                } else {
+                    resultado = 0;
+                }
+            }
+            return resultado;
+        }
+    }
+   
 }
 
